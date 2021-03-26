ef79e48ebb27214f4dd0d6449bfe6ff2fa3ac7e1
lcymu <50705646+lcymu@users.noreply.github.com>
2021-03-26 08:39:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.69.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.69.6/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 rm -rf package/lean/luci-theme-argon
